d1df6479dcdae86bbaf58d476d7b79f3230a8287
cloudforet-admin <admin@cloudforet.io>
2023-06-11 15:51:43 +0000

    [CI] api has been built

--- dist/python/spaceone/api/plugin/v1/plugin_pb2.py
+++ dist/python/spaceone/api/plugin/v1/plugin_pb2.py
@@ -17,7 +17,7 @@ from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n#spaceone/api/plugin/v1/plugin.proto\x12\x16spaceone.api.plugin.v1\x1a\x1bgoogle/protobuf/empty.proto\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto\"\xed\x01\n\x15PluginEndpointRequest\x12\x11\n\tplugin_id\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\x12\'\n\x06labels\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\tdomain_id\x18\x04 \x01(\t\x12O\n\x0cupgrade_mode\x18\x05 \x01(\x0e\x32\x39.spaceone.api.plugin.v1.PluginEndpointRequest.UpgradeMode\"#\n\x0bUpgradeMode\x12\n\n\x06MANUAL\x10\x00\x12\x08\n\x04\x41UTO\x10\x01\"x\n\x15PluginMetadataRequest\x12\x11\n\tplugin_id\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\x12(\n\x07options\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\tdomain_id\x18\x04 \x01(\t\"d\n\x14PluginFailureRequest\x12\x15\n\rsupervisor_id\x18\x01 \x01(\t\x12\x11\n\tplugin_id\x18\x02 \x01(\t\x12\x0f\n\x07version\x18\x03 \x01(\t\x12\x11\n\tdomain_id\x18\x04 \x01(\t\"Q\n\x0ePluginEndpoint\x12\x10\n\x08\x65ndpoint\x18\x01 \x01(\t\x12\x14\n\x0c\x61\x63\x63\x65ss_token\x18\x02 \x01(\t\x12\x17\n\x0fupdated_version\x18\x03 \x01(\t\";\n\x0ePluginMetadata\x12)\n\x08metadata\x18\x01 \x01(\x0b\x32\x17.google.protobuf.Struct2\xc9\x03\n\x06Plugin\x12\x9e\x01\n\x13get_plugin_endpoint\x12-.spaceone.api.plugin.v1.PluginEndpointRequest\x1a&.spaceone.api.plugin.v1.PluginEndpoint\"0\x82\xd3\xe4\x93\x02*\"%/plugin/v1/plugin/get-plugin-endpoint:\x01*\x12\x97\x01\n\x13get_plugin_metadata\x12-.spaceone.api.plugin.v1.PluginMetadataRequest\x1a&.spaceone.api.plugin.v1.PluginMetadata\")\x82\xd3\xe4\x93\x02#\"\x1e/plugin/v1/plugin/get-metadata:\x01*\x12\x83\x01\n\x0enotify_failure\x12,.spaceone.api.plugin.v1.PluginFailureRequest\x1a\x16.google.protobuf.Empty\"+\x82\xd3\xe4\x93\x02%\" /plugin/v1/plugin/notify-failure:\x01*B=Z;github.com/cloudforet-io/api/dist/go/spaceone/api/plugin/v1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n#spaceone/api/plugin/v1/plugin.proto\x12\x16spaceone.api.plugin.v1\x1a\x1bgoogle/protobuf/empty.proto\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto\"\xed\x01\n\x15PluginEndpointRequest\x12\x11\n\tplugin_id\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\x12\'\n\x06labels\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\tdomain_id\x18\x04 \x01(\t\x12O\n\x0cupgrade_mode\x18\x05 \x01(\x0e\x32\x39.spaceone.api.plugin.v1.PluginEndpointRequest.UpgradeMode\"#\n\x0bUpgradeMode\x12\n\n\x06MANUAL\x10\x00\x12\x08\n\x04\x41UTO\x10\x01\"\xee\x01\n\x15PluginMetadataRequest\x12\x11\n\tplugin_id\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\x12(\n\x07options\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\tdomain_id\x18\x04 \x01(\t\x12O\n\x0cupgrade_mode\x18\x05 \x01(\x0e\x32\x39.spaceone.api.plugin.v1.PluginMetadataRequest.UpgradeMode\"#\n\x0bUpgradeMode\x12\n\n\x06MANUAL\x10\x00\x12\x08\n\x04\x41UTO\x10\x01\"d\n\x14PluginFailureRequest\x12\x15\n\rsupervisor_id\x18\x01 \x01(\t\x12\x11\n\tplugin_id\x18\x02 \x01(\t\x12\x0f\n\x07version\x18\x03 \x01(\t\x12\x11\n\tdomain_id\x18\x04 \x01(\t\"Q\n\x0ePluginEndpoint\x12\x10\n\x08\x65ndpoint\x18\x01 \x01(\t\x12\x14\n\x0c\x61\x63\x63\x65ss_token\x18\x02 \x01(\t\x12\x17\n\x0fupdated_version\x18\x03 \x01(\t\";\n\x0ePluginMetadata\x12)\n\x08metadata\x18\x01 \x01(\x0b\x32\x17.google.protobuf.Struct2\xc9\x03\n\x06Plugin\x12\x9e\x01\n\x13get_plugin_endpoint\x12-.spaceone.api.plugin.v1.PluginEndpointRequest\x1a&.spaceone.api.plugin.v1.PluginEndpoint\"0\x82\xd3\xe4\x93\x02*\"%/plugin/v1/plugin/get-plugin-endpoint:\x01*\x12\x97\x01\n\x13get_plugin_metadata\x12-.spaceone.api.plugin.v1.PluginMetadataRequest\x1a&.spaceone.api.plugin.v1.PluginMetadata\")\x82\xd3\xe4\x93\x02#\"\x1e/plugin/v1/plugin/get-metadata:\x01*\x12\x83\x01\n\x0enotify_failure\x12,.spaceone.api.plugin.v1.PluginFailureRequest\x1a\x16.google.protobuf.Empty\"+\x82\xd3\xe4\x93\x02%\" /plugin/v1/plugin/notify-failure:\x01*B=Z;github.com/cloudforet-io/api/dist/go/spaceone/api/plugin/v1b\x06proto3')
 
 
 
@@ -27,6 +27,7 @@ _PLUGINFAILUREREQUEST = DESCRIPTOR.message_types_by_name['PluginFailureRequest']
 _PLUGINENDPOINT = DESCRIPTOR.message_types_by_name['PluginEndpoint']
 _PLUGINMETADATA = DESCRIPTOR.message_types_by_name['PluginMetadata']
 _PLUGINENDPOINTREQUEST_UPGRADEMODE = _PLUGINENDPOINTREQUEST.enum_types_by_name['UpgradeMode']
+_PLUGINMETADATAREQUEST_UPGRADEMODE = _PLUGINMETADATAREQUEST.enum_types_by_name['UpgradeMode']
 PluginEndpointRequest = _reflection.GeneratedProtocolMessageType('PluginEndpointRequest', (_message.Message,), {
   'DESCRIPTOR' : _PLUGINENDPOINTREQUEST,
   '__module__' : 'spaceone.api.plugin.v1.plugin_pb2'
@@ -77,14 +78,16 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _PLUGINENDPOINTREQUEST._serialized_end=390
   _PLUGINENDPOINTREQUEST_UPGRADEMODE._serialized_start=355
   _PLUGINENDPOINTREQUEST_UPGRADEMODE._serialized_end=390
-  _PLUGINMETADATAREQUEST._serialized_start=392
-  _PLUGINMETADATAREQUEST._serialized_end=512
-  _PLUGINFAILUREREQUEST._serialized_start=514
-  _PLUGINFAILUREREQUEST._serialized_end=614
-  _PLUGINENDPOINT._serialized_start=616
-  _PLUGINENDPOINT._serialized_end=697
-  _PLUGINMETADATA._serialized_start=699
-  _PLUGINMETADATA._serialized_end=758
-  _PLUGIN._serialized_start=761
-  _PLUGIN._serialized_end=1218
+  _PLUGINMETADATAREQUEST._serialized_start=393
+  _PLUGINMETADATAREQUEST._serialized_end=631
+  _PLUGINMETADATAREQUEST_UPGRADEMODE._serialized_start=355
+  _PLUGINMETADATAREQUEST_UPGRADEMODE._serialized_end=390
+  _PLUGINFAILUREREQUEST._serialized_start=633
+  _PLUGINFAILUREREQUEST._serialized_end=733
+  _PLUGINENDPOINT._serialized_start=735
+  _PLUGINENDPOINT._serialized_end=816
+  _PLUGINMETADATA._serialized_start=818
+  _PLUGINMETADATA._serialized_end=877
+  _PLUGIN._serialized_start=880
+  _PLUGIN._serialized_end=1337
 # @@protoc_insertion_point(module_scope)
