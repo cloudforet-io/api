bfeb0a150152adf81c9f18d8b36088ab15fc7e7b
ImMin5 <mino@megazone.com>
2023-09-26 16:14:23 +0900

    feat: add common schema dirs
    Signed-off-by: ImMin5 <mino@megazone.com>
    (cherry picked from commit 3914cb6d8e4b3234597b21918cf4426a2823a40c)

--- bin/openapi.py
+++ bin/openapi.py
@@ -1,12 +1,18 @@
+import functools
 import glob
 import json
 import os
 import sys
 
+import click
 from jinja2 import Environment, FileSystemLoader
 
+PROJECT = 'cloudforet'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+OUTPUT_DIR = os.path.join(BASE_DIR, 'dist', 'openapi')
+JSON_DIR = os.path.join(BASE_DIR, 'dist', 'json', PROJECT, 'api')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
+DEFAULT_COMMON_SCHEMA_DIR = os.path.join(BASE_DIR, 'dist', 'json', PROJECT, 'api', 'core', 'v1')
 
 
 def _error(msg):
@@ -49,6 +55,7 @@ def _modify_message_field_type_python_to_openapi(message) -> dict:
 
 
 def _set_required_or_optional(field_list) -> list:
+    fields = {}
     for field in field_list:
         field_description = field['description']
         if field_description.split('\n')[-1].lower().strip() == '+optional':
@@ -69,7 +76,7 @@ def _set_enum_field_as_string(field_list, enums) -> list:
 
 
 def _sort_fields_by_required(field_list) -> list:
-    return sorted(field_list, key=lambda x: x['required'], reverse=True)
+    return sorted(field_list, key=lambda x: x['required'] != 'True')
 
 
 def _make_table_description(field_list) -> str:
@@ -123,13 +130,10 @@ def _transform_to_filtered_method(method):
         return method
 
 
-def _make_openapi_json(output_dir, service_name, core_message_list):
-    openapi_file_name = f'{service_name}_openapi.json'
-    output_path = os.path.join(output_dir, 'openapi', openapi_file_name)
-
-    output_dir_with_service_name = os.path.join(output_dir, 'json', 'cloudforet', 'api', service_name)
+def _make_openapi_json(target, common_schema_list, sub_dir, output_dir=None):
     template_dir = os.path.join(TEMPLATE_DIR, 'openapi')
-    json_file_path_list = _get_json_file_paths(output_dir_with_service_name)
+    output_dir = output_dir if output_dir else sub_dir.replace('json', 'openapi')
+    json_file_path_list = _get_json_file_paths(sub_dir)
 
     filtered_method_list = []
     filtered_messages = {}
@@ -142,7 +146,7 @@ def _make_openapi_json(output_dir, service_name, core_message_list):
             scalar_value_types = _get_scalar_value_types(json_data.get('scalarValueTypes', []))
             service_list = json_data.get('files')[0].get('services', [])
             message_list = json_data.get('files')[0].get('messages', [])
-            message_list.extend(core_message_list)
+            message_list.extend(common_schema_list)
 
             for message in message_list:
                 field_list = message.get('fields')
@@ -151,8 +155,8 @@ def _make_openapi_json(output_dir, service_name, core_message_list):
                 message = _modify_message_field_type_python_to_openapi(message)
                 message['fields'] = _set_required_or_optional(field_list)
                 message['fields'] = _set_enum_field_as_string(field_list, enums)
-                message['fields'] = _sort_fields_by_required(field_list)
-                message['table_description'] = _make_table_description(field_list)
+                sorted_field_list = _sort_fields_by_required(field_list)
+                message['table_description'] = _make_table_description(sorted_field_list)
                 filtered_messages.update({message.get('name'): message})
 
             for service in service_list:
@@ -164,54 +168,78 @@ def _make_openapi_json(output_dir, service_name, core_message_list):
     if filtered_method_list:
         jinja_env = Environment(autoescape=False, loader=FileSystemLoader(template_dir), trim_blocks=True)
         template = jinja_env.get_template('openapi.json.tmpl')
-        content = template.render(methods=filtered_method_list, messages=filtered_messages, service_name=service_name,
+        content = template.render(methods=filtered_method_list, messages=filtered_messages, service_name=target,
                                   scalar_value_types=scalar_value_types)
 
-        with open(output_path, 'w') as f:
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir, exist_ok=True)
+
+        with open(f'{output_dir}/openapi.json', 'w') as f:
             f.write(content)
 
 
-def _get_json_file_paths(dist_json_dir) -> list:
-    return [json_file for json_file in glob.iglob(os.path.join(dist_json_dir, '**', '*.json'), recursive=True)
-            if f'{dist_json_dir}/plugin' not in json_file]
+def _get_json_file_paths(json_input_file_path) -> list:
+    return [json_file for json_file in glob.iglob(os.path.join(json_input_file_path, '**', '*.json'), recursive=True)]
 
 
-def _get_core_messages(output_dir) -> list:
-    core_message_list = []
-    output_dir_with_service = os.path.join(output_dir, 'json', 'cloudforet', 'api', 'core')
-    json_file_path_list = _get_json_file_paths(output_dir_with_service)
+def _get_common_schemas(common_schema_dir_list) -> list:
+    common_schema_list = []
+    common_schema_path_list = []
 
-    for json_file_path in json_file_path_list:
-        with open(json_file_path, 'r') as f:
+    for common_schema_dir in common_schema_dir_list:
+        common_schema_path_list.extend(_get_json_file_paths(common_schema_dir))
+
+    for common_schema_path in common_schema_path_list:
+        with open(common_schema_path, 'r') as f:
             json_data = json.load(f)
             messages = json_data.get('files')[0].get('messages')
-            core_message_list.extend(messages)
+            common_schema_list.extend(messages)
 
-    return core_message_list
+    return common_schema_list
 
 
-def build_openapi_json(output_dir, service, debug=False):
-    json_file_path = os.path.join(output_dir, 'json', service)
+def _get_json_input_sub_dirs(json_input_dir) -> list:
+    json_input_sub_dir_list = []
+    for dir_name in os.listdir(os.path.join(json_input_dir)):
+        json_input_sub_dir_name = os.path.join(json_input_dir, dir_name)
+        if os.path.isdir(json_input_sub_dir_name):
+            json_input_sub_dir_list.append(json_input_sub_dir_name)
+    return json_input_sub_dir_list
+
+
+def build_openapi_json(**params):
+    target = params['target'].replace('-', '_')
+    debug = params.get('debug', False)
+    output_dir = params.get('output_dir')
+    json_input_dir = params.get('json_dir') if params.get('json_dir') else JSON_DIR
+
+    common_schema_dir_list = params.get('common_schema_dirs', [DEFAULT_COMMON_SCHEMA_DIR])
+
     try:
-        core_message_list = _get_core_messages(output_dir)
-        _make_openapi_json(output_dir, service, core_message_list)
+        common_schema_list = _get_common_schemas(common_schema_dir_list)
+
+        sub_dir_list = _get_json_input_sub_dirs(f'{json_input_dir}/{target}')
+        list(map(functools.partial(_make_openapi_json, target, common_schema_list, output_dir=output_dir), sub_dir_list))
 
         if debug:
             print()
-            print(f'{json_file_path}')
+            print(f'[DEBUG] Directory list : {sub_dir_list}')
 
     except Exception as e:
-        _error(f"Failed to OpenAPI Compile : {json_file_path}\n{e}")
+        _error(f"Failed to OpenAPI Compile : {json_input_dir}\n{e}")
 
-    print(f"[SUCCESS] OpenAPI Compile : {json_file_path}")
+    print(f"[SUCCESS] OpenAPI Compile : {json_input_dir}/{target}")
+
+
+@click.command()
+@click.option('-t', '--target', type=str, help='Target service name (ex. identity, inventory, cost-analysis, etc.)')
+@click.option('-j', '--json-dir', type=str, help='Input json directory', default=JSON_DIR)
+@click.option('-o', '--output-dir', type=str, help='If not specified, the default output directory is decided by the json_dir')
+@click.option('-s', '--common-schema-dirs', type=str, help='Common schema directories', multiple=True, default=[])
+@click.option('-d', '--debug', is_flag=True, help='Debug mode')
+def openapi(**params):
+    build_openapi_json(**params)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        _error("Usage: python openapi.py {output_dir:str} {service:srt} {debug: bool(optional)}")
-
-    output_dir = sys.argv[1]
-    service = sys.argv[2]
-    debug = False if len(sys.argv) == 4 else sys.argv[3]
-
-    build_openapi_json(output_dir=output_dir, service=service, debug=debug)
+    openapi()
