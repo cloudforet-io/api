5c32057564d6866aa140ae889a291176330006ce
cloudforet-admin <admin@cloudforet.io>
2023-12-14 21:40:00 +0000

    [CI] api has been built

--- dist/python/spaceone/api/identity/v2/role_pb2.py
+++ dist/python/spaceone/api/identity/v2/role_pb2.py
@@ -18,7 +18,7 @@ from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 from spaceone.api.core.v2 import query_pb2 as spaceone_dot_api_dot_core_dot_v2_dot_query__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n#spaceone/api/identity/v2/role.proto\x12\x18spaceone.api.identity.v2\x1a\x1bgoogle/protobuf/empty.proto\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto\x1a spaceone/api/core/v2/query.proto\"\x8d\x01\n\nPageAccess\x12\x0c\n\x04page\x18\x01 \x01(\t\x12\x43\n\npermission\x18\x02 \x01(\x0e\x32/.spaceone.api.identity.v2.PageAccess.Permission\",\n\nPermission\x12\x08\n\x04NONE\x10\x00\x12\x08\n\x04VIEW\x10\x01\x12\n\n\x06MANAGE\x10\x02\"\xcf\x01\n\x11\x43reateRoleRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x35\n\trole_type\x18\x02 \x01(\x0e\x32\".spaceone.api.identity.v2.RoleType\x12\x13\n\x0bpermissions\x18\x03 \x03(\t\x12\x39\n\x0bpage_access\x18\x04 \x03(\x0b\x32$.spaceone.api.identity.v2.PageAccess\x12%\n\x04tags\x18\x05 \x01(\x0b\x32\x17.google.protobuf.Struct\"\xa9\x01\n\x11UpdateRoleRequest\x12\x0f\n\x07role_id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x13\n\x0bpermissions\x18\x03 \x03(\t\x12\x39\n\x0bpage_access\x18\x04 \x03(\x0b\x32$.spaceone.api.identity.v2.PageAccess\x12%\n\x04tags\x18\x05 \x01(\x0b\x32\x17.google.protobuf.Struct\"\x1e\n\x0bRoleRequest\x12\x0f\n\x07role_id\x18\x01 \x01(\t\"\xa6\x02\n\x08RoleInfo\x12\x0f\n\x07role_id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x35\n\trole_type\x18\x03 \x01(\x0e\x32\".spaceone.api.identity.v2.RoleType\x12\x13\n\x0bpermissions\x18\x04 \x03(\t\x12\x39\n\x0bpage_access\x18\x05 \x03(\x0b\x32$.spaceone.api.identity.v2.PageAccess\x12%\n\x04tags\x18\x06 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x12\n\nis_managed\x18\x07 \x01(\x08\x12\x11\n\tdomain_id\x18\x15 \x01(\t\x12\x12\n\ncreated_at\x18\x1f \x01(\t\x12\x12\n\nupdated_at\x18  \x01(\t\"\x93\x01\n\x0fRoleSearchQuery\x12*\n\x05query\x18\x01 \x01(\x0b\x32\x1b.spaceone.api.core.v2.Query\x12\x0f\n\x07role_id\x18\x02 \x01(\t\x12\x0c\n\x04name\x18\x03 \x01(\t\x12\x35\n\trole_type\x18\x04 \x01(\x0e\x32\".spaceone.api.identity.v2.RoleType\"U\n\tRolesInfo\x12\x33\n\x07results\x18\x01 \x03(\x0b\x32\".spaceone.api.identity.v2.RoleInfo\x12\x13\n\x0btotal_count\x18\x02 \x01(\x05\"E\n\rRoleStatQuery\x12\x34\n\x05query\x18\x01 \x01(\x0b\x32%.spaceone.api.core.v2.StatisticsQuery*o\n\x08RoleType\x12\x08\n\x04NONE\x10\x00\x12\n\n\x06SYSTEM\x10\x01\x12\x10\n\x0cSYSTEM_ADMIN\x10\x02\x12\x10\n\x0c\x44OMAIN_ADMIN\x10\x03\x12\x13\n\x0fWORKSPACE_OWNER\x10\x04\x12\x14\n\x10WORKSPACE_MEMBER\x10\x05\x32\xd0\x05\n\x04Role\x12~\n\x06\x63reate\x12+.spaceone.api.identity.v2.CreateRoleRequest\x1a\".spaceone.api.identity.v2.RoleInfo\"#\x82\xd3\xe4\x93\x02\x1d\"\x18/identity/v2/role/create:\x01*\x12~\n\x06update\x12+.spaceone.api.identity.v2.UpdateRoleRequest\x1a\".spaceone.api.identity.v2.RoleInfo\"#\x82\xd3\xe4\x93\x02\x1d\"\x18/identity/v2/role/update:\x01*\x12l\n\x06\x64\x65lete\x12%.spaceone.api.identity.v2.RoleRequest\x1a\x16.google.protobuf.Empty\"#\x82\xd3\xe4\x93\x02\x1d\"\x18/identity/v2/role/delete:\x01*\x12r\n\x03get\x12%.spaceone.api.identity.v2.RoleRequest\x1a\".spaceone.api.identity.v2.RoleInfo\" \x82\xd3\xe4\x93\x02\x1a\"\x15/identity/v2/role/get:\x01*\x12y\n\x04list\x12).spaceone.api.identity.v2.RoleSearchQuery\x1a#.spaceone.api.identity.v2.RolesInfo\"!\x82\xd3\xe4\x93\x02\x1b\"\x16/identity/v2/role/list:\x01*\x12k\n\x04stat\x12\'.spaceone.api.identity.v2.RoleStatQuery\x1a\x17.google.protobuf.Struct\"!\x82\xd3\xe4\x93\x02\x1b\"\x16/identity/v2/role/stat:\x01*B?Z=github.com/cloudforet-io/api/dist/go/spaceone/api/identity/v2b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n#spaceone/api/identity/v2/role.proto\x12\x18spaceone.api.identity.v2\x1a\x1bgoogle/protobuf/empty.proto\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto\x1a spaceone/api/core/v2/query.proto\"\xa9\x01\n\x11\x43reateRoleRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x35\n\trole_type\x18\x02 \x01(\x0e\x32\".spaceone.api.identity.v2.RoleType\x12\x13\n\x0bpermissions\x18\x03 \x03(\t\x12\x13\n\x0bpage_access\x18\x04 \x03(\t\x12%\n\x04tags\x18\x05 \x01(\x0b\x32\x17.google.protobuf.Struct\"\x83\x01\n\x11UpdateRoleRequest\x12\x0f\n\x07role_id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x13\n\x0bpermissions\x18\x03 \x03(\t\x12\x13\n\x0bpage_access\x18\x04 \x03(\t\x12%\n\x04tags\x18\x05 \x01(\x0b\x32\x17.google.protobuf.Struct\"\x1e\n\x0bRoleRequest\x12\x0f\n\x07role_id\x18\x01 \x01(\t\"\x80\x02\n\x08RoleInfo\x12\x0f\n\x07role_id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x35\n\trole_type\x18\x03 \x01(\x0e\x32\".spaceone.api.identity.v2.RoleType\x12\x13\n\x0bpermissions\x18\x04 \x03(\t\x12\x13\n\x0bpage_access\x18\x05 \x03(\t\x12%\n\x04tags\x18\x06 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x12\n\nis_managed\x18\x07 \x01(\x08\x12\x11\n\tdomain_id\x18\x15 \x01(\t\x12\x12\n\ncreated_at\x18\x1f \x01(\t\x12\x12\n\nupdated_at\x18  \x01(\t\"\x93\x01\n\x0fRoleSearchQuery\x12*\n\x05query\x18\x01 \x01(\x0b\x32\x1b.spaceone.api.core.v2.Query\x12\x0f\n\x07role_id\x18\x02 \x01(\t\x12\x0c\n\x04name\x18\x03 \x01(\t\x12\x35\n\trole_type\x18\x04 \x01(\x0e\x32\".spaceone.api.identity.v2.RoleType\"U\n\tRolesInfo\x12\x33\n\x07results\x18\x01 \x03(\x0b\x32\".spaceone.api.identity.v2.RoleInfo\x12\x13\n\x0btotal_count\x18\x02 \x01(\x05\"E\n\rRoleStatQuery\x12\x34\n\x05query\x18\x01 \x01(\x0b\x32%.spaceone.api.core.v2.StatisticsQuery*o\n\x08RoleType\x12\x08\n\x04NONE\x10\x00\x12\n\n\x06SYSTEM\x10\x01\x12\x10\n\x0cSYSTEM_ADMIN\x10\x02\x12\x10\n\x0c\x44OMAIN_ADMIN\x10\x03\x12\x13\n\x0fWORKSPACE_OWNER\x10\x04\x12\x14\n\x10WORKSPACE_MEMBER\x10\x05\x32\xd0\x05\n\x04Role\x12~\n\x06\x63reate\x12+.spaceone.api.identity.v2.CreateRoleRequest\x1a\".spaceone.api.identity.v2.RoleInfo\"#\x82\xd3\xe4\x93\x02\x1d\"\x18/identity/v2/role/create:\x01*\x12~\n\x06update\x12+.spaceone.api.identity.v2.UpdateRoleRequest\x1a\".spaceone.api.identity.v2.RoleInfo\"#\x82\xd3\xe4\x93\x02\x1d\"\x18/identity/v2/role/update:\x01*\x12l\n\x06\x64\x65lete\x12%.spaceone.api.identity.v2.RoleRequest\x1a\x16.google.protobuf.Empty\"#\x82\xd3\xe4\x93\x02\x1d\"\x18/identity/v2/role/delete:\x01*\x12r\n\x03get\x12%.spaceone.api.identity.v2.RoleRequest\x1a\".spaceone.api.identity.v2.RoleInfo\" \x82\xd3\xe4\x93\x02\x1a\"\x15/identity/v2/role/get:\x01*\x12y\n\x04list\x12).spaceone.api.identity.v2.RoleSearchQuery\x1a#.spaceone.api.identity.v2.RolesInfo\"!\x82\xd3\xe4\x93\x02\x1b\"\x16/identity/v2/role/list:\x01*\x12k\n\x04stat\x12\'.spaceone.api.identity.v2.RoleStatQuery\x1a\x17.google.protobuf.Struct\"!\x82\xd3\xe4\x93\x02\x1b\"\x16/identity/v2/role/stat:\x01*B?Z=github.com/cloudforet-io/api/dist/go/spaceone/api/identity/v2b\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -38,26 +38,22 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _globals['_ROLE'].methods_by_name['list']._serialized_options = b'\202\323\344\223\002\033\"\026/identity/v2/role/list:\001*'
   _globals['_ROLE'].methods_by_name['stat']._options = None
   _globals['_ROLE'].methods_by_name['stat']._serialized_options = b'\202\323\344\223\002\033\"\026/identity/v2/role/stat:\001*'
-  _globals['_ROLETYPE']._serialized_start=1351
-  _globals['_ROLETYPE']._serialized_end=1462
-  _globals['_PAGEACCESS']._serialized_start=189
-  _globals['_PAGEACCESS']._serialized_end=330
-  _globals['_PAGEACCESS_PERMISSION']._serialized_start=286
-  _globals['_PAGEACCESS_PERMISSION']._serialized_end=330
-  _globals['_CREATEROLEREQUEST']._serialized_start=333
-  _globals['_CREATEROLEREQUEST']._serialized_end=540
-  _globals['_UPDATEROLEREQUEST']._serialized_start=543
-  _globals['_UPDATEROLEREQUEST']._serialized_end=712
-  _globals['_ROLEREQUEST']._serialized_start=714
-  _globals['_ROLEREQUEST']._serialized_end=744
-  _globals['_ROLEINFO']._serialized_start=747
-  _globals['_ROLEINFO']._serialized_end=1041
-  _globals['_ROLESEARCHQUERY']._serialized_start=1044
-  _globals['_ROLESEARCHQUERY']._serialized_end=1191
-  _globals['_ROLESINFO']._serialized_start=1193
-  _globals['_ROLESINFO']._serialized_end=1278
-  _globals['_ROLESTATQUERY']._serialized_start=1280
-  _globals['_ROLESTATQUERY']._serialized_end=1349
-  _globals['_ROLE']._serialized_start=1465
-  _globals['_ROLE']._serialized_end=2185
+  _globals['_ROLETYPE']._serialized_start=1093
+  _globals['_ROLETYPE']._serialized_end=1204
+  _globals['_CREATEROLEREQUEST']._serialized_start=189
+  _globals['_CREATEROLEREQUEST']._serialized_end=358
+  _globals['_UPDATEROLEREQUEST']._serialized_start=361
+  _globals['_UPDATEROLEREQUEST']._serialized_end=492
+  _globals['_ROLEREQUEST']._serialized_start=494
+  _globals['_ROLEREQUEST']._serialized_end=524
+  _globals['_ROLEINFO']._serialized_start=527
+  _globals['_ROLEINFO']._serialized_end=783
+  _globals['_ROLESEARCHQUERY']._serialized_start=786
+  _globals['_ROLESEARCHQUERY']._serialized_end=933
+  _globals['_ROLESINFO']._serialized_start=935
+  _globals['_ROLESINFO']._serialized_end=1020
+  _globals['_ROLESTATQUERY']._serialized_start=1022
+  _globals['_ROLESTATQUERY']._serialized_end=1091
+  _globals['_ROLE']._serialized_start=1207
+  _globals['_ROLE']._serialized_end=1927
 # @@protoc_insertion_point(module_scope)
