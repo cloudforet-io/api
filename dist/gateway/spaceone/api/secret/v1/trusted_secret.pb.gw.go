bee7c4b1f8e1c9992ce1baf5d35703518e7c14c3
cloudforet-admin <admin@cloudforet.io>
2024-03-13 06:42:17 +0000

    [CI] api has been built

--- dist/gateway/spaceone/api/secret/v1/trusted_secret.pb.gw.go
+++ dist/gateway/spaceone/api/secret/v1/trusted_secret.pb.gw.go
@@ -136,6 +136,32 @@ func local_request_TrustedSecret_UpdateData_0(ctx context.Context, marshaler run
 
 }
 
+func request_TrustedSecret_GetData_0(ctx context.Context, marshaler runtime.Marshaler, client extV1.TrustedSecretClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq extV1.GetTrustedSecretDataRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetData(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_TrustedSecret_GetData_0(ctx context.Context, marshaler runtime.Marshaler, server extV1.TrustedSecretServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq extV1.GetTrustedSecretDataRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetData(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_TrustedSecret_Get_0(ctx context.Context, marshaler runtime.Marshaler, client extV1.TrustedSecretClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq extV1.TrustedSecretRequest
 	var metadata runtime.ServerMetadata
@@ -317,6 +343,31 @@ func RegisterTrustedSecretHandlerServer(ctx context.Context, mux *runtime.ServeM
 		}
 
 		forward_TrustedSecret_UpdateData_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_TrustedSecret_GetData_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/spaceone.api.secret.v1.TrustedSecret/GetData", runtime.WithHTTPPathPattern("/spaceone.api.secret.v1.TrustedSecret/get_data"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_TrustedSecret_GetData_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TrustedSecret_GetData_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -524,6 +575,28 @@ func RegisterTrustedSecretHandlerClient(ctx context.Context, mux *runtime.ServeM
 
 	})
 
+	mux.Handle("POST", pattern_TrustedSecret_GetData_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/spaceone.api.secret.v1.TrustedSecret/GetData", runtime.WithHTTPPathPattern("/spaceone.api.secret.v1.TrustedSecret/get_data"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_TrustedSecret_GetData_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TrustedSecret_GetData_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_TrustedSecret_Get_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -602,6 +675,8 @@ var (
 
 	pattern_TrustedSecret_UpdateData_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"secret", "v1", "trusted-secret", "update-data"}, ""))
 
+	pattern_TrustedSecret_GetData_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"spaceone.api.secret.v1.TrustedSecret", "get_data"}, ""))
+
 	pattern_TrustedSecret_Get_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"secret", "v1", "trusted-secret", "get"}, ""))
 
 	pattern_TrustedSecret_List_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"secret", "v1", "trusted-secret", "list"}, ""))
@@ -617,6 +692,8 @@ var (
 	forward_TrustedSecret_Delete_0 = runtime.ForwardResponseMessage
 
 	forward_TrustedSecret_UpdateData_0 = runtime.ForwardResponseMessage
+
+	forward_TrustedSecret_GetData_0 = runtime.ForwardResponseMessage
 
 	forward_TrustedSecret_Get_0 = runtime.ForwardResponseMessage
 
--- dist/go.mod
+++ dist/go.mod
@@ -7,7 +7,7 @@ replace github.com/cloudforet-io/api => ./
 require (
 	github.com/golang/protobuf v1.5.4
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1
-	google.golang.org/genproto/googleapis/api v0.0.0-20240304212257-790db918fca8
+	google.golang.org/genproto/googleapis/api v0.0.0-20240311173647-c811ad7063a7
 	google.golang.org/grpc v1.62.1
 	google.golang.org/protobuf v1.33.0
 )
@@ -16,5 +16,5 @@ require (
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240228224816-df926f6c8641 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240311132316-a219d84964c2 // indirect
 )
