686631c508bb29b8a0b857ea4c18bd457fdba85c
cloudforet-admin <admin@cloudforet.io>
2024-06-02 09:00:22 +0000

    [CI] api has been built

--- dist/python/build/lib/spaceone/api/dashboard/v1/private_folder_pb2.py
+++ dist/python/build/lib/spaceone/api/dashboard/v1/private_folder_pb2.py
@@ -18,7 +18,7 @@ from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 from spaceone.api.core.v2 import query_pb2 as spaceone_dot_api_dot_core_dot_v2_dot_query__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n.spaceone/api/dashboard/v1/private_folder.proto\x12\x19spaceone.api.dashboard.v1\x1a\x1bgoogle/protobuf/empty.proto\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto\x1a spaceone/api/core/v2/query.proto\"\xd0\x01\n\x1a\x43reatePrivateFolderRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12%\n\x04tags\x18\x02 \x01(\x0b\x32\x17.google.protobuf.Struct\x12+\n\ndashboards\x18\x03 \x03(\x0b\x32\x17.google.protobuf.Struct\"P\n\rResourceGroup\x12\x17\n\x13RESOURCE_GROUP_NONE\x10\x00\x12\n\n\x06\x44OMAIN\x10\x01\x12\r\n\tWORKSPACE\x10\x02\x12\x0b\n\x07PROJECT\x10\x03\"d\n\x1aUpdatePrivateFolderRequest\x12\x11\n\tfolder_id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12%\n\x04tags\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\")\n\x14PrivateFolderRequest\x12\x11\n\tfolder_id\x18\x01 \x01(\t\"a\n\x12PrivateFolderQuery\x12*\n\x05query\x18\x01 \x01(\x0b\x32\x1b.spaceone.api.core.v2.Query\x12\x11\n\tfolder_id\x18\x02 \x01(\t\x12\x0c\n\x04name\x18\x03 \x01(\t\"\xa7\x01\n\x11PrivateFolderInfo\x12\x11\n\tfolder_id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12%\n\x04tags\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\tdomain_id\x18\x15 \x01(\t\x12\x0f\n\x07user_id\x18\x16 \x01(\t\x12\x12\n\ncreated_at\x18\x1f \x01(\t\x12\x12\n\nupdated_at\x18  \x01(\t\"h\n\x12PrivateFoldersInfo\x12=\n\x07results\x18\x01 \x03(\x0b\x32,.spaceone.api.dashboard.v1.PrivateFolderInfo\x12\x13\n\x0btotal_count\x18\x02 \x01(\x05\"N\n\x16PrivateFolderStatQuery\x12\x34\n\x05query\x18\x01 \x01(\x0b\x32%.spaceone.api.core.v2.StatisticsQuery2\xff\x06\n\rPrivateFolder\x12\x9d\x01\n\x06\x63reate\x12\x35.spaceone.api.dashboard.v1.CreatePrivateFolderRequest\x1a,.spaceone.api.dashboard.v1.PrivateFolderInfo\".\x82\xd3\xe4\x93\x02(\"#/dashboard/v1/private-folder/create:\x01*\x12\x9d\x01\n\x06update\x12\x35.spaceone.api.dashboard.v1.UpdatePrivateFolderRequest\x1a,.spaceone.api.dashboard.v1.PrivateFolderInfo\".\x82\xd3\xe4\x93\x02(\"#/dashboard/v1/private-folder/update:\x01*\x12\x81\x01\n\x06\x64\x65lete\x12/.spaceone.api.dashboard.v1.PrivateFolderRequest\x1a\x16.google.protobuf.Empty\".\x82\xd3\xe4\x93\x02(\"#/dashboard/v1/private-folder/delete:\x01*\x12\x91\x01\n\x03get\x12/.spaceone.api.dashboard.v1.PrivateFolderRequest\x1a,.spaceone.api.dashboard.v1.PrivateFolderInfo\"+\x82\xd3\xe4\x93\x02%\" /dashboard/v1/private-folder/get:\x01*\x12\x92\x01\n\x04list\x12-.spaceone.api.dashboard.v1.PrivateFolderQuery\x1a-.spaceone.api.dashboard.v1.PrivateFoldersInfo\",\x82\xd3\xe4\x93\x02&\"!/dashboard/v1/private-folder/list:\x01*\x12\x80\x01\n\x04stat\x12\x31.spaceone.api.dashboard.v1.PrivateFolderStatQuery\x1a\x17.google.protobuf.Struct\",\x82\xd3\xe4\x93\x02&\"!/dashboard/v1/private-folder/stat:\x01*B@Z>github.com/cloudforet-io/api/dist/go/spaceone/api/dashboard/v1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n.spaceone/api/dashboard/v1/private_folder.proto\x12\x19spaceone.api.dashboard.v1\x1a\x1bgoogle/protobuf/empty.proto\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto\x1a spaceone/api/core/v2/query.proto\"\x94\x01\n\x1a\x43reatePrivateFolderRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12%\n\x04tags\x18\x02 \x01(\x0b\x32\x17.google.protobuf.Struct\x12+\n\ndashboards\x18\x03 \x03(\x0b\x32\x17.google.protobuf.Struct\x12\x14\n\x0cworkspace_id\x18\x04 \x01(\t\"d\n\x1aUpdatePrivateFolderRequest\x12\x11\n\tfolder_id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12%\n\x04tags\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\")\n\x14PrivateFolderRequest\x12\x11\n\tfolder_id\x18\x01 \x01(\t\"w\n\x12PrivateFolderQuery\x12*\n\x05query\x18\x01 \x01(\x0b\x32\x1b.spaceone.api.core.v2.Query\x12\x11\n\tfolder_id\x18\x02 \x01(\t\x12\x0c\n\x04name\x18\x03 \x01(\t\x12\x14\n\x0cworkspace_id\x18\x04 \x01(\t\"\xbd\x01\n\x11PrivateFolderInfo\x12\x11\n\tfolder_id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12%\n\x04tags\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\tdomain_id\x18\x15 \x01(\t\x12\x14\n\x0cworkspace_id\x18\x16 \x01(\t\x12\x0f\n\x07user_id\x18\x17 \x01(\t\x12\x12\n\ncreated_at\x18\x1f \x01(\t\x12\x12\n\nupdated_at\x18  \x01(\t\"h\n\x12PrivateFoldersInfo\x12=\n\x07results\x18\x01 \x03(\x0b\x32,.spaceone.api.dashboard.v1.PrivateFolderInfo\x12\x13\n\x0btotal_count\x18\x02 \x01(\x05\"N\n\x16PrivateFolderStatQuery\x12\x34\n\x05query\x18\x01 \x01(\x0b\x32%.spaceone.api.core.v2.StatisticsQuery2\xff\x06\n\rPrivateFolder\x12\x9d\x01\n\x06\x63reate\x12\x35.spaceone.api.dashboard.v1.CreatePrivateFolderRequest\x1a,.spaceone.api.dashboard.v1.PrivateFolderInfo\".\x82\xd3\xe4\x93\x02(\"#/dashboard/v1/private-folder/create:\x01*\x12\x9d\x01\n\x06update\x12\x35.spaceone.api.dashboard.v1.UpdatePrivateFolderRequest\x1a,.spaceone.api.dashboard.v1.PrivateFolderInfo\".\x82\xd3\xe4\x93\x02(\"#/dashboard/v1/private-folder/update:\x01*\x12\x81\x01\n\x06\x64\x65lete\x12/.spaceone.api.dashboard.v1.PrivateFolderRequest\x1a\x16.google.protobuf.Empty\".\x82\xd3\xe4\x93\x02(\"#/dashboard/v1/private-folder/delete:\x01*\x12\x91\x01\n\x03get\x12/.spaceone.api.dashboard.v1.PrivateFolderRequest\x1a,.spaceone.api.dashboard.v1.PrivateFolderInfo\"+\x82\xd3\xe4\x93\x02%\" /dashboard/v1/private-folder/get:\x01*\x12\x92\x01\n\x04list\x12-.spaceone.api.dashboard.v1.PrivateFolderQuery\x1a-.spaceone.api.dashboard.v1.PrivateFoldersInfo\",\x82\xd3\xe4\x93\x02&\"!/dashboard/v1/private-folder/list:\x01*\x12\x80\x01\n\x04stat\x12\x31.spaceone.api.dashboard.v1.PrivateFolderStatQuery\x1a\x17.google.protobuf.Struct\",\x82\xd3\xe4\x93\x02&\"!/dashboard/v1/private-folder/stat:\x01*B@Z>github.com/cloudforet-io/api/dist/go/spaceone/api/dashboard/v1b\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -39,21 +39,19 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_PRIVATEFOLDER'].methods_by_name['stat']._loaded_options = None
   _globals['_PRIVATEFOLDER'].methods_by_name['stat']._serialized_options = b'\202\323\344\223\002&\"!/dashboard/v1/private-folder/stat:\001*'
   _globals['_CREATEPRIVATEFOLDERREQUEST']._serialized_start=201
-  _globals['_CREATEPRIVATEFOLDERREQUEST']._serialized_end=409
-  _globals['_CREATEPRIVATEFOLDERREQUEST_RESOURCEGROUP']._serialized_start=329
-  _globals['_CREATEPRIVATEFOLDERREQUEST_RESOURCEGROUP']._serialized_end=409
-  _globals['_UPDATEPRIVATEFOLDERREQUEST']._serialized_start=411
-  _globals['_UPDATEPRIVATEFOLDERREQUEST']._serialized_end=511
-  _globals['_PRIVATEFOLDERREQUEST']._serialized_start=513
-  _globals['_PRIVATEFOLDERREQUEST']._serialized_end=554
-  _globals['_PRIVATEFOLDERQUERY']._serialized_start=556
-  _globals['_PRIVATEFOLDERQUERY']._serialized_end=653
-  _globals['_PRIVATEFOLDERINFO']._serialized_start=656
-  _globals['_PRIVATEFOLDERINFO']._serialized_end=823
-  _globals['_PRIVATEFOLDERSINFO']._serialized_start=825
-  _globals['_PRIVATEFOLDERSINFO']._serialized_end=929
-  _globals['_PRIVATEFOLDERSTATQUERY']._serialized_start=931
-  _globals['_PRIVATEFOLDERSTATQUERY']._serialized_end=1009
-  _globals['_PRIVATEFOLDER']._serialized_start=1012
-  _globals['_PRIVATEFOLDER']._serialized_end=1907
+  _globals['_CREATEPRIVATEFOLDERREQUEST']._serialized_end=349
+  _globals['_UPDATEPRIVATEFOLDERREQUEST']._serialized_start=351
+  _globals['_UPDATEPRIVATEFOLDERREQUEST']._serialized_end=451
+  _globals['_PRIVATEFOLDERREQUEST']._serialized_start=453
+  _globals['_PRIVATEFOLDERREQUEST']._serialized_end=494
+  _globals['_PRIVATEFOLDERQUERY']._serialized_start=496
+  _globals['_PRIVATEFOLDERQUERY']._serialized_end=615
+  _globals['_PRIVATEFOLDERINFO']._serialized_start=618
+  _globals['_PRIVATEFOLDERINFO']._serialized_end=807
+  _globals['_PRIVATEFOLDERSINFO']._serialized_start=809
+  _globals['_PRIVATEFOLDERSINFO']._serialized_end=913
+  _globals['_PRIVATEFOLDERSTATQUERY']._serialized_start=915
+  _globals['_PRIVATEFOLDERSTATQUERY']._serialized_end=993
+  _globals['_PRIVATEFOLDER']._serialized_start=996
+  _globals['_PRIVATEFOLDER']._serialized_end=1891
 # @@protoc_insertion_point(module_scope)
