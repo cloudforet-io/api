d0dc1bc63b637db7b6f3531c76845e542425f14a
cloudforet-admin <admin@cloudforet.io>
2024-12-23 13:39:27 +0000

    [CI] api has been built

--- dist/go.mod
+++ dist/go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.25.1
 	google.golang.org/genproto/googleapis/api v0.0.0-20241219192143-6b3ec007d9bb
 	google.golang.org/grpc v1.69.2
-	google.golang.org/protobuf v1.36.0
+	google.golang.org/protobuf v1.36.1
 )
 
 require (
--- dist/python/build/lib/spaceone/api/config/v1/public_config_pb2.py
+++ dist/python/build/lib/spaceone/api/config/v1/public_config_pb2.py
@@ -18,7 +18,7 @@ from google.api import annotations_pb2 as google_dot_api_dot_annotations__pb2
 from spaceone.api.core.v2 import query_pb2 as spaceone_dot_api_dot_core_dot_v2_dot_query__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n*spaceone/api/config/v1/public_config.proto\x12\x16spaceone.api.config.v1\x1a\x1bgoogle/protobuf/empty.proto\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto\x1a spaceone/api/core/v2/query.proto\"t\n\x16SetPublicConfigRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12%\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x17.google.protobuf.Struct\x12%\n\x04tags\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\"#\n\x13PublicConfigRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\"S\n\x17PublicConfigSearchQuery\x12*\n\x05query\x18\x01 \x01(\x0b\x32\x1b.spaceone.api.core.v2.Query\x12\x0c\n\x04name\x18\x02 \x01(\t\"\xa9\x01\n\x10PublicConfigInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12%\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x17.google.protobuf.Struct\x12%\n\x04tags\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\tdomain_id\x18\x15 \x01(\t\x12\x12\n\ncreated_at\x18\x1f \x01(\t\x12\x12\n\nupdated_at\x18  \x01(\t\"c\n\x11PublicConfigsInfo\x12\x39\n\x07results\x18\x01 \x03(\x0b\x32(.spaceone.api.config.v1.PublicConfigInfo\x12\x13\n\x0btotal_count\x18\x02 \x01(\x05\"M\n\x15PublicConfigStatQuery\x12\x34\n\x05query\x18\x01 \x01(\x0b\x32%.spaceone.api.core.v2.StatisticsQuery2\xcd\x06\n\x0cPublicConfig\x12\x8e\x01\n\x06\x63reate\x12..spaceone.api.config.v1.SetPublicConfigRequest\x1a(.spaceone.api.config.v1.PublicConfigInfo\"*\x82\xd3\xe4\x93\x02$\"\x1f/config/v1/public-config/create:\x01*\x12\x8e\x01\n\x06update\x12..spaceone.api.config.v1.SetPublicConfigRequest\x1a(.spaceone.api.config.v1.PublicConfigInfo\"*\x82\xd3\xe4\x93\x02$\"\x1f/config/v1/public-config/update:\x01*\x12\x88\x01\n\x03set\x12..spaceone.api.config.v1.SetPublicConfigRequest\x1a(.spaceone.api.config.v1.PublicConfigInfo\"\'\x82\xd3\xe4\x93\x02!\"\x1c/config/v1/public-config/set:\x01*\x12y\n\x06\x64\x65lete\x12+.spaceone.api.config.v1.PublicConfigRequest\x1a\x16.google.protobuf.Empty\"*\x82\xd3\xe4\x93\x02$\"\x1f/config/v1/public-config/delete:\x01*\x12\x85\x01\n\x03get\x12+.spaceone.api.config.v1.PublicConfigRequest\x1a(.spaceone.api.config.v1.PublicConfigInfo\"\'\x82\xd3\xe4\x93\x02!\"\x1c/config/v1/public-config/get:\x01*\x12\x8c\x01\n\x04list\x12/.spaceone.api.config.v1.PublicConfigSearchQuery\x1a).spaceone.api.config.v1.PublicConfigsInfo\"(\x82\xd3\xe4\x93\x02\"\"\x1d/config/v1/public-config/list:\x01*B=Z;github.com/cloudforet-io/api/dist/go/spaceone/api/config/v1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n*spaceone/api/config/v1/public_config.proto\x12\x16spaceone.api.config.v1\x1a\x1bgoogle/protobuf/empty.proto\x1a\x1cgoogle/protobuf/struct.proto\x1a\x1cgoogle/api/annotations.proto\x1a spaceone/api/core/v2/query.proto\"t\n\x16SetPublicConfigRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12%\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x17.google.protobuf.Struct\x12%\n\x04tags\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\"6\n\x13PublicConfigRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x11\n\tdomain_id\x18\x02 \x01(\t\"f\n\x17PublicConfigSearchQuery\x12*\n\x05query\x18\x01 \x01(\x0b\x32\x1b.spaceone.api.core.v2.Query\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x11\n\tdomain_id\x18\x15 \x01(\t\"\xa9\x01\n\x10PublicConfigInfo\x12\x0c\n\x04name\x18\x01 \x01(\t\x12%\n\x04\x64\x61ta\x18\x02 \x01(\x0b\x32\x17.google.protobuf.Struct\x12%\n\x04tags\x18\x03 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x11\n\tdomain_id\x18\x15 \x01(\t\x12\x12\n\ncreated_at\x18\x1f \x01(\t\x12\x12\n\nupdated_at\x18  \x01(\t\"c\n\x11PublicConfigsInfo\x12\x39\n\x07results\x18\x01 \x03(\x0b\x32(.spaceone.api.config.v1.PublicConfigInfo\x12\x13\n\x0btotal_count\x18\x02 \x01(\x05\"M\n\x15PublicConfigStatQuery\x12\x34\n\x05query\x18\x01 \x01(\x0b\x32%.spaceone.api.core.v2.StatisticsQuery2\xcd\x06\n\x0cPublicConfig\x12\x8e\x01\n\x06\x63reate\x12..spaceone.api.config.v1.SetPublicConfigRequest\x1a(.spaceone.api.config.v1.PublicConfigInfo\"*\x82\xd3\xe4\x93\x02$\"\x1f/config/v1/public-config/create:\x01*\x12\x8e\x01\n\x06update\x12..spaceone.api.config.v1.SetPublicConfigRequest\x1a(.spaceone.api.config.v1.PublicConfigInfo\"*\x82\xd3\xe4\x93\x02$\"\x1f/config/v1/public-config/update:\x01*\x12\x88\x01\n\x03set\x12..spaceone.api.config.v1.SetPublicConfigRequest\x1a(.spaceone.api.config.v1.PublicConfigInfo\"\'\x82\xd3\xe4\x93\x02!\"\x1c/config/v1/public-config/set:\x01*\x12y\n\x06\x64\x65lete\x12+.spaceone.api.config.v1.PublicConfigRequest\x1a\x16.google.protobuf.Empty\"*\x82\xd3\xe4\x93\x02$\"\x1f/config/v1/public-config/delete:\x01*\x12\x85\x01\n\x03get\x12+.spaceone.api.config.v1.PublicConfigRequest\x1a(.spaceone.api.config.v1.PublicConfigInfo\"\'\x82\xd3\xe4\x93\x02!\"\x1c/config/v1/public-config/get:\x01*\x12\x8c\x01\n\x04list\x12/.spaceone.api.config.v1.PublicConfigSearchQuery\x1a).spaceone.api.config.v1.PublicConfigsInfo\"(\x82\xd3\xe4\x93\x02\"\"\x1d/config/v1/public-config/list:\x01*B=Z;github.com/cloudforet-io/api/dist/go/spaceone/api/config/v1b\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -41,15 +41,15 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_SETPUBLICCONFIGREQUEST']._serialized_start=193
   _globals['_SETPUBLICCONFIGREQUEST']._serialized_end=309
   _globals['_PUBLICCONFIGREQUEST']._serialized_start=311
-  _globals['_PUBLICCONFIGREQUEST']._serialized_end=346
-  _globals['_PUBLICCONFIGSEARCHQUERY']._serialized_start=348
-  _globals['_PUBLICCONFIGSEARCHQUERY']._serialized_end=431
-  _globals['_PUBLICCONFIGINFO']._serialized_start=434
-  _globals['_PUBLICCONFIGINFO']._serialized_end=603
-  _globals['_PUBLICCONFIGSINFO']._serialized_start=605
-  _globals['_PUBLICCONFIGSINFO']._serialized_end=704
-  _globals['_PUBLICCONFIGSTATQUERY']._serialized_start=706
-  _globals['_PUBLICCONFIGSTATQUERY']._serialized_end=783
-  _globals['_PUBLICCONFIG']._serialized_start=786
-  _globals['_PUBLICCONFIG']._serialized_end=1631
+  _globals['_PUBLICCONFIGREQUEST']._serialized_end=365
+  _globals['_PUBLICCONFIGSEARCHQUERY']._serialized_start=367
+  _globals['_PUBLICCONFIGSEARCHQUERY']._serialized_end=469
+  _globals['_PUBLICCONFIGINFO']._serialized_start=472
+  _globals['_PUBLICCONFIGINFO']._serialized_end=641
+  _globals['_PUBLICCONFIGSINFO']._serialized_start=643
+  _globals['_PUBLICCONFIGSINFO']._serialized_end=742
+  _globals['_PUBLICCONFIGSTATQUERY']._serialized_start=744
+  _globals['_PUBLICCONFIGSTATQUERY']._serialized_end=821
+  _globals['_PUBLICCONFIG']._serialized_start=824
+  _globals['_PUBLICCONFIG']._serialized_end=1669
 # @@protoc_insertion_point(module_scope)
